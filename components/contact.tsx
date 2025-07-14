37544f23c1d8e8cff90bcba10c0f550ff001d0d0
Rohan Padhye <68507017+rohanpadhye-cmd@users.noreply.github.com>
2025-07-14 13:37:40 +0530

    Update contact.tsx

--- components/contact.tsx
+++ components/contact.tsx
@@ -33,8 +33,8 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:rohan.padhye@outlook.com">
-          rohan.padhye@outlook.com
+        <a className="underline" href="mailto:rohanpadhye.dev@gmail.com">
+          rohanpadhye.dev@gmail.com
         </a>{" "}
         or through this form.
       </p>
